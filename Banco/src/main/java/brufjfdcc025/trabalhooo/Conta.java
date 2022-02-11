c8d9e1e69f710223ea126b6e86ce41ce561bca3c
marianaricha <mariana.richa@estudante.ufjf.br>
2022-02-11 15:23:27 -0300

    atualiza conta

--- Banco/src/main/java/brufjfdcc025/trabalhooo/Conta.java
+++ Banco/src/main/java/brufjfdcc025/trabalhooo/Conta.java
@@ -13,47 +13,38 @@ public class Conta {
     Scanner teclado = new Scanner(System.in);
     
     private List <Extrato> extrato;
-    private String tipo, numero, senha;
+    private String numero;
     private float saldo;
-    private Cartao cartao;
+    
 
     public Conta() {
-        cartao = new Cartao();
+        
         extrato = new ArrayList<>();
     }
 
-    public String getTipo() {
-        return tipo;
-    }
+    
 
     public String getNumero() {
         return numero;
     }
 
-    public String getSenha() {
-        return senha;
-    }
+    
 
     public float getSaldo() {
         return saldo;
     }
-
-    public Cartao getCartao() {
-        return cartao;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
     
-
-    public void setSenha(String senha) {
-        this.senha = senha;
+    public void addExtrato(String tipo, float valor, Date data){
+        extrato.add(new Extrato(tipo,valor,data));
     }
     
-    
-    //funcionalidades do programa
+    public String imprimeExtrato(){
+        String imprime="";
+        for (Extrato e : extrato){
+            imprime+="\n"+e;
+        }
+        return imprime;
+    }
     
     private void setSaldo(float saldo) {
         this.saldo = saldo;
@@ -64,8 +55,7 @@ public class Conta {
     
     
     /*implementar funções:
-    criar cartao
-    renovar cartao
+  
     emprestimos
     
     */
