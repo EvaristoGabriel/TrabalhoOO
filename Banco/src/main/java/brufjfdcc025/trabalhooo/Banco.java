260c8f3a137ee1b2abc1cebe678bedbb8b15c5c8
marianaricha <mariana.richa@estudante.ufjf.br>
2022-02-11 15:19:04 -0300

    atualiza banco

--- Banco/src/main/java/brufjfdcc025/trabalhooo/Banco.java
+++ Banco/src/main/java/brufjfdcc025/trabalhooo/Banco.java
@@ -2,10 +2,12 @@ package brufjfdcc025.trabalhooo;
 
 import brufjfdcc025.trabalhooo.control.JSON;
 import brufjfdcc025.trabalhooo.view.Menu;
+import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 import javax.swing.JOptionPane;
+
 
 /*
         Trabalho de Orientação a Objetos
@@ -17,6 +19,7 @@ public class Banco {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        System.out.println(Calendar.getInstance().getTime());
 
         Set<Cliente> clientes = new HashSet<>(); //conjunto de clientes 
 
